a64663bf4476884009e13ef37298c6d417ca0604
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-05 18:11:13 +0200

    PASS//

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import app from "./server.js"
-import mongoose from "mongoose"
+import app from "./server.js";
+import mongoose from "mongoose";
 
-const port = process.env.PORT || 3001
-const mongoUrl = process.env.MONGO_URL
+const port = process.env.PORT || 3001;
+const mongoUrl = process.env.MONGO_URL;
 
-mongoose.connect(mongoUrl, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("🍀 Connected to MongoDB")
-    app.listen(port, () => {
-        console.log("🤘 Server listening on port " + port)
-    })
-})
+// mongoose
+//   .connect(mongoUrl, {
+//     useNewUrlParser: true,
+//   })
+//   .then(() => {
+//     console.log("🍀 Connected to MongoDB");
+//     app.listen(port, () => {
+//       console.log("🤘 Server listening on port " + port);
+//     });
+//   });
+
+app.listen(port, async () => {
+  try {
+    await mongoose.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`✅ Server is running on ${port}  and connected to db`);
+  } catch (error) {
+    console.log("Db connection is failed ", error);
+  }
+});
+
+app.on("error", (error) =>
+  console.log(`❌ Server is not running due to : ${error}`)
+);
