44fd4bb4cb9b1061c95e37fb1468a0e90436dfa6
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-05 15:04:21 +0200

    product routes

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -1,26 +1,53 @@
-import express from "express"
-import ProductModel from "./model.js"
+import express from "express";
+import ProductModel from "./model.js";
 
-const productsRouter = express.Router()
+const productsRouter = express.Router();
 
-productsRouter.post('/', async (req, res) => {
-    // create a new product.
-    try {
-        const newProduct = new ProductModel(req.body)
-        await newProduct.save()
-        res.status(201).send(newProduct)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
+productsRouter.post("/", async (req, res) => {
+  // create a new product.
+  try {
+    const newProduct = new ProductModel(req.body);
+    await newProduct.save();
+    res.status(201).send(newProduct);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
-productsRouter.get('/:id', async (req, res) => {
-    try {
-        const product = await ProductModel.findById(req.params.id)
-        res.status(200).send(product)
-    } catch (error) {
-        res.status(404).send(error)
-    }
-})
+productsRouter.get("/", async (req, res) => {
+  try {
+    const product = await ProductModel.find();
+    res.status(200).send(product);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
 
-export default productsRouter
+productsRouter.get("/:id", async (req, res) => {
+  try {
+    const product = await ProductModel.findById(req.params.id);
+    res.status(200).send(product);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+productsRouter.put("/:id", async (req, res) => {
+  try {
+    const product = await ProductModel.findByIdAndUpdate(req.params.id);
+    res.status(200).send(product);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+productsRouter.delete("/:id", async (req, res) => {
+  try {
+    const product = await ProductModel.findByIdAndDelete(req.params.id);
+    res.status(200).send(`${req.params.id} has gone for good!`);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+export default productsRouter;
